94257bdce3f4b3970a1c54b3fe1991d4b00abe00
john <pratheepjohn5@gmail.com>
2026-04-02 10:04:17 +0530

    new changes

--- chat/views.py
+++ chat/views.py
@@ -1,52 +1,45 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-
-
-
-
-
 import os 
 from dotenv import load_dotenv
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-
 import google.generativeai as genai
 
 load_dotenv()
 
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key="AIzaSyDftBZEbnGsSBBH17iBzMjW5VsWkP4Dv0w")
 
-model=genai.Generativemodel("gemini-2.5-flash")
+
+
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
 
 @csrf_exempt
-
-
-def chat_view(request):
-    if request.method=="POST":
+def chat(request):
+    if request.method == "POST":
         try:
-            body:json.loads(request.body)
-                        user_message = body.get("message", "")
-prompt=f"""
+            body = json.loads(request.body)
+
+            user_message = body.get("message", "")
+
+            prompt = f"""
 You are a shopping assistant for NxtTrendz.
 
-Use the below data to answer:
-
+User message:
 {user_message}
 """
 
-response=model.generate_content(prompt)
-return jsonResponse({
+            response = model.generate_content(prompt)
 
-    "reply":response.reply
-})
+            return JsonResponse({
+                "reply": response.text
+            })
 
-except Exception as e:
-return jsonResponse({
+        except Exception as e:
+            return JsonResponse({
+                "reply": "Error: " + str(e)
+            })
 
-    "reply":"Error: "+str(e)
-})
     return JsonResponse({"error": "Only POST allowed"})
-
